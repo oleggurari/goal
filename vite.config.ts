7db762db177c021aede9c709e9b9bf8189c417c2
oleggurari <oleg@oleggurari.com>
2025-11-21 16:15:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
+export default defineConfig({
+  base: '/goal/',
+  plugins: [react()],
+})
       base: "/goal/",   // 👈 ОБЯЗАТЕЛЬНО до server
 
       server: {
